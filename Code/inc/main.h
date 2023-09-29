32cf037f29421d8c948ab31127cf13830aee5f42
Pavlo Skirko <pavlo.skirko@gmail.com>
2023-09-30 00:51:32 +0300

    Added drawing a number function
    Signed-off-by: Pavlo Skirko <pavlo.skirko@gmail.com>

--- Code/inc/main.h
+++ Code/inc/main.h
@@ -1,15 +1,9 @@
 #ifndef MAIN
 #define MAIN
 
+#include "stm32f4xx.h"
 
-#define SYSCLCK 16000000
-
-
-uint64_t get_time_ms();
-void RCC_Init (void);
-void GPIO_Init (void);
-void TIM4_Init (void);
-void TIM1_PWM_Init(void);
-void delay (uint32_t counter);
+void SSD1306_UpdateScreen(void);
+void draw_a_number(uint8_t numb);
 
 #endif
